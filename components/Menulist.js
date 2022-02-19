130f999b5b6d2e8ce7660f4ed918a9b41afae8e8
Kurnia Muhamad <omenkzz@gmail.com>
2022-02-19 21:13:20 +0700

    Update Menu page style

--- components/Menulist.js
+++ components/Menulist.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
-import { Box, FlatList, Center, NativeBaseProvider, Text } from "native-base";
+import { Box, FlatList, Heading, HStack, VStack, Text, Spacer, Center, NativeBaseProvider, Image } from "native-base";
+
 
 export default function Menulist() {
   const [data, setData] = useState([]);
@@ -14,9 +15,32 @@ export default function Menulist() {
 
   const renderItem = ({ item }) => {
     return (
-      <Box px={5} py={2} rounded="md" bg="primary.300" my={2}>
-        {item.name}
-      </Box>
+      <Box borderBottomWidth="1" _dark={{
+        borderColor: "gray.600"
+      }} borderColor="coolGray.200" pl="4" pr="5" py="2">
+          <HStack space={3} justifyContent="space-between">
+            <Image size="48px" source={{
+              uri: "https://wallpaperaccess.com/full/317501.jpg"}} alt="Alternate Text" />
+            <VStack>
+              <Text _dark={{
+                color: "warmGray.50"
+              }} color="coolGray.800" bold>
+                {item.name}
+              </Text>
+              <Text color="coolGray.600" _dark={{
+                color: "warmGray.200"
+              }}>
+                {item.price}
+              </Text>
+            </VStack>
+            <Spacer />
+            <Text fontSize="xs" _dark={{
+              color: "warmGray.50"
+            }} color="coolGray.800" alignSelf="flex-start">
+              {item.created_at}
+            </Text>
+          </HStack>
+        </Box>
     );
   };
 
@@ -26,7 +50,10 @@ export default function Menulist() {
   
   return (
     <NativeBaseProvider>
-      <Center flex={1}>
+      <Center flex={1} pt="10">
+        <Heading fontSize="xl" p="4" pb="3">
+          Menu
+        </Heading>
         {loading && <Box>Loading..</Box>}
         {data && (
           <FlatList
